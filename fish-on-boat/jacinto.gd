a47d1944e28ced4af8b7d9ca953fc825550e3c01
Marcelo <166082569+VarBus@users.noreply.github.com>
2025-04-11 14:07:17 -0500

    fead: add boat movement

--- fish-on-boat/jacinto.gd
+++ fish-on-boat/jacinto.gd
@@ -8,8 +8,13 @@ var sliding = false
 var last_direction = 1 
 var slide_direction = 0
 @onready var slide_timer: Timer = $slide_timer
+var controlling_boat = false
 
 func _physics_process(delta: float) -> void:
+	if controlling_boat:
+		velocity = Vector2.ZERO
+		move_and_slide()
+		return
 	var direction = Vector2.ZERO
 	if not sliding :
 		if Input.is_action_pressed("right"):
