52cadc741de05a11be135b12e082cc8ca29bb325
Marcelo <166082569+VarBus@users.noreply.github.com>
2025-04-25 14:39:06 -0500

    fix: inventory problems

--- fish-on-boat/fishing_rod_area.gd
+++ fish-on-boat/fishing_rod_area.gd
@@ -1,8 +1,10 @@
 extends Area2D
 
-@export var item_scene: PackedScene  # 👉 Esto debe ser la escena funcional, como la "fishing_rod.tscn"
+signal item_selected(item_scene)
+const ITEM_SCENE := preload("res://fishing_rod.tscn")
 
 func _input_event(viewport, event, shape_idx):
+	var item_instance = ITEM_SCENE.instantiate()
 	if event is InputEventMouseButton and event.pressed:
 		print("🎯 Ítem del inventario clickeado")
-		emit_signal("item_selected", item_scene)
+		emit_signal("item_selected", item_instance)
